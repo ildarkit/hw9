d03553dda174f34b1a5646e7e25fcce02effaac8
ILdar Shamiev <shamief@yandex.ru>
2018-03-28 16:32:51 +0300

    init memc_load_conc

---
+++ memc_load_conc.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import gzip
+import glob
+import queue
+import logging
+import threading
+import collections
+import multiprocessing as mp
+from optparse import OptionParser
+
+# pip install python-memcached
+import memcache
+# brew install protobuf
+# protoc  --python_out=. ./appsinstalled.proto
+# pip install protobuf
+import appsinstalled_pb2
+
+NORMAL_ERR_RATE = 0.01
+AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
+
+
+class Worker(threading.Thread):
+    pass
+
+
+def dot_rename(path):
+    head, fn = os.path.split(path)
+    # atomic in most cases
+    os.rename(path, os.path.join(head, "." + fn))
+
+
+def insert_appsinstalled(memc_addr, appsinstalled, dry_run=False):
+    ua = appsinstalled_pb2.UserApps()
+    ua.lat = appsinstalled.lat
+    ua.lon = appsinstalled.lon
+    key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
+    ua.apps.extend(appsinstalled.apps)
+    packed = ua.SerializeToString()
+    # @TODO persistent connection
+    # @TODO retry and timeouts!
+    try:
+        if dry_run:
+            logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
+        else:
+            memc = memcache.Client([memc_addr])
+            memc.set(key, packed)
+    except Exception as e:
+        logging.exception("Cannot write to memc %s: %s" % (memc_addr, e))
+        return False
+    return True
+
+
+def parse_appsinstalled(line):
+    line_parts = line.strip().split("\t")
+    if len(line_parts) < 5:
+        return
+    dev_type, dev_id, lat, lon, raw_apps = line_parts
+    if not dev_type or not dev_id:
+        return
+    try:
+        apps = [int(a.strip()) for a in raw_apps.split(",")]
+    except ValueError:
+        apps = [int(a.strip()) for a in raw_apps.split(",") if a.isidigit()]
+        logging.info("Not all user apps are digits: `%s`" % line)
+    try:
+        lat, lon = float(lat), float(lon)
+    except ValueError:
+        logging.info("Invalid geo coords: `%s`" % line)
+    return AppsInstalled(dev_type, dev_id, lat, lon, apps)
+
+
+def main(options):
+    device_memc = {
+        "idfa": options.idfa,
+        "gaid": options.gaid,
+        "adid": options.adid,
+        "dvid": options.dvid,
+    }
+    pass
+
+
+def prototest():
+    sample = "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
+    for line in sample.splitlines():
+        dev_type, dev_id, lat, lon, raw_apps = line.strip().split("\t")
+        apps = [int(a) for a in raw_apps.split(",") if a.isdigit()]
+        lat, lon = float(lat), float(lon)
+        ua = appsinstalled_pb2.UserApps()
+        ua.lat = lat
+        ua.lon = lon
+        ua.apps.extend(apps)
+        packed = ua.SerializeToString()
+        unpacked = appsinstalled_pb2.UserApps()
+        unpacked.ParseFromString(packed)
+        assert ua == unpacked
+
+
+if __name__ == '__main__':
+    op = OptionParser()
+    op.add_option("-t", "--test", action="store_true", default=False)
+    op.add_option("-l", "--log", action="store", default=None)
+    op.add_option("--dry", action="store_true", default=False)
+    op.add_option("--pattern", action="store", default="/data/appsinstalled/*.tsv.gz")
+    op.add_option("--idfa", action="store", default="127.0.0.1:33013")
+    op.add_option("--gaid", action="store", default="127.0.0.1:33014")
+    op.add_option("--adid", action="store", default="127.0.0.1:33015")
+    op.add_option("--dvid", action="store", default="127.0.0.1:33016")
+    op.add_option("-w", "--workers", action="store", default=4)
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    if opts.test:
+        prototest()
+        sys.exit(0)
+
+    logging.info("Memc loader started with options: %s" % opts)
+    try:
+        main(opts)
+    except Exception as e:
+        logging.exception("Unexpected error: %s" % e)
+        sys.exit(1)
